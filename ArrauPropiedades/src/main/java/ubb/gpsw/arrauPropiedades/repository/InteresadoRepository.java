10086982269b592ebc9b54d676db43ec5953cb7f
Camila <camilapinto@outlook.cl>
2019-05-24 20:00:37 -0400

    CRUD Completo 100% funcional
    CRUD implementado a la clase Interesado. Funcionando totalmente, conectado junto a la base de datos.

--- ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/repository/InteresadoRepository.java
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/repository/InteresadoRepository.java
@@ -1,11 +1,9 @@
 package ubb.gpsw.arrauPropiedades.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import ubb.gpsw.arrauPropiedades.model.Interesado;
 
-@Repository
 public interface InteresadoRepository extends CrudRepository<Interesado, Integer>{
 
 }
