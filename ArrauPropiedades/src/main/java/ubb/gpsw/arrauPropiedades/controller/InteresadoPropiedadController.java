0f197160d1c786698bd40316de092594184fc68c
Camila <camilapinto@outlook.cl>
2019-05-25 22:57:07 -0400

    CRUD + "Vista" de InteresadoPropiedad
    Considerar: modificar bien las vistas por el tema de eliminar los datos de esta entidad. El agregar listar y editar funcionan 100% bien.

---
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/controller/InteresadoPropiedadController.java
@@ -0,0 +1,48 @@
+package ubb.gpsw.arrauPropiedades.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ubb.gpsw.arrauPropiedades.model.InteresadoPropiedad;
+import ubb.gpsw.arrauPropiedades.service.InteresadoPropiedadService;
+
+@Controller
+public class InteresadoPropiedadController {
+	
+	@Autowired
+	private InteresadoPropiedadService intPropService;
+	
+	@RequestMapping("/indexIP")
+	public String index(Model model) {
+		model.addAttribute("list",intPropService.getAll());
+		return "indexIP";
+	}
+	
+	@GetMapping("/saveIP/{id}")
+	public String showSave(@PathVariable("id") int id, Model model) {
+		if(id!=0) {
+			model.addAttribute("interesadoPropiedad",intPropService.get(id));
+		} else {
+			model.addAttribute("interesadoPropiedad", new InteresadoPropiedad());
+		}
+		return "saveIP";
+	}
+	
+	@PostMapping("/saveIP")
+	public String save(InteresadoPropiedad interesadoPropiedad, Model model) {
+		intPropService.save(interesadoPropiedad);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/deleteIP/{id}")
+	public String delete(@PathVariable Integer id, Model model) {
+		intPropService.delete(id);
+		return "redirect:/";
+	}
+
+}
---
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/repository/InteresadoPropiedadRepository.java
@@ -0,0 +1,9 @@
+package ubb.gpsw.arrauPropiedades.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ubb.gpsw.arrauPropiedades.model.InteresadoPropiedad;
+
+public interface InteresadoPropiedadRepository extends CrudRepository<InteresadoPropiedad, Integer>{
+
+}
