149279e30ce8441fc641632588d3b6672e231c0f
Camila <camilapinto@outlook.cl>
2019-05-27 12:56:27 -0400

    CRUD InteresadoPropiedad
    (de nuevo)

---
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/service/InteresadoPropiedadService.java
@@ -0,0 +1,8 @@
+package ubb.gpsw.arrauPropiedades.service;
+
+import ubb.gpsw.arrauPropiedades.model.InteresadoPropiedad;
+import ubb.gpsw.arrauPropiedades.utilidades.Respuesta;
+
+public interface InteresadoPropiedadService extends Respuesta<InteresadoPropiedad, Integer> {
+
+}
