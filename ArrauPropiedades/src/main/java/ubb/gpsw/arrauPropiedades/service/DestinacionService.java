6f50cc27848a98485a00fa0086ab6f545cbb213c
AbrahamBriones <abraham.briones1501@alumnos.ubiobio.cl>
2019-05-27 12:40:00 -0400

    Solución del CRUD Destinación
    Vuelvo a subir el CRUD destinación debido a la eliminación que se produjo

---
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/service/DestinacionService.java
@@ -0,0 +1,8 @@
+package ubb.gpsw.arrauPropiedades.service;
+
+import ubb.gpsw.arrauPropiedades.model.Destinacion;
+import ubb.gpsw.arrauPropiedades.utilidades.Respuesta;
+
+public interface DestinacionService extends Respuesta<Destinacion, Integer> {
+
+}
