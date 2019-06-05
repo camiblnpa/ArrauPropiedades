8cdfb330a13ff15a35c14979d0a1f2ad74a23b59
Camila <camilapinto@outlook.cl>
2019-06-04 23:59:28 -0400

    Vista 2.0
    Se implementa la vista realizada del boceto

---
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/controller/IndexController.java
@@ -0,0 +1,23 @@
+package ubb.gpsw.arrauPropiedades.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/indexAdmin")
+	public String indexAdmin(Model model, HttpSession session){
+		return "indexAdmin";
+	}
+	
+	@GetMapping("/index")
+	public String index(Model model, HttpSession session) {
+		return "index";
+	}
+
+
+}
