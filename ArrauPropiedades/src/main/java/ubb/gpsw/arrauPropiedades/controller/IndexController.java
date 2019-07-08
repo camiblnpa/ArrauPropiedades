24623f22ee9419ac4412123da0823ca8acaf07a7
Camila <camilapinto@outlook.cl>
2019-07-08 00:37:57 -0400

    Modificación algunas rutas + Intento de implementación del catálogo

--- ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/controller/IndexController.java
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/controller/IndexController.java
@@ -2,12 +2,37 @@ package ubb.gpsw.arrauPropiedades.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ubb.gpsw.arrauPropiedades.service.PropiedadService;
+import ubb.gpsw.arrauPropiedades.model.Propiedad;
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private PropiedadService propService;
+	
+	@RequestMapping("/index")
+	public String list(Model model) {
+		model.addAttribute("list",propService.getAll());
+		return "index";
+	}
+	
+	@GetMapping("/propiedadDetalle/{id}")
+	public String getPropiedadById(Model model, @PathVariable("id") Integer id) {
+		if(id!=0) {
+			model.addAttribute("listDetalle", propService.get(id));
+		} else {
+			System.out.println("error");
+		}
+		return "propiedadDetalleCliente";
+	}
 	
 	@GetMapping("/indexAdmin")
 	public String indexAdmin(Model model, HttpSession session){
@@ -18,5 +43,7 @@ public class IndexController {
 	public String index(Model model, HttpSession session) {
 		return "index";
 	}
+	
+
 	
 }
