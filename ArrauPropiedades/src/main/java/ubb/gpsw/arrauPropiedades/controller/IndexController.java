fc92cca593beec68137f4682534cf54fa28014ed
Camila <camilapinto@outlook.cl>
2019-07-11 11:48:41 -0400

    Catálogo funcionando

--- ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/controller/IndexController.java
+++ ArrauPropiedades/src/main/java/ubb/gpsw/arrauPropiedades/controller/IndexController.java
@@ -1,49 +1,44 @@
 package ubb.gpsw.arrauPropiedades.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ubb.gpsw.arrauPropiedades.service.PropiedadService;
 import ubb.gpsw.arrauPropiedades.model.Propiedad;
+import ubb.gpsw.arrauPropiedades.service.PropiedadService;
 
 @Controller
 public class IndexController {
 	
 	@Autowired
 	private PropiedadService propService;
-	
-	@RequestMapping("/index")
-	public String list(Model model) {
-		model.addAttribute("list",propService.getAll());
-		return "index";
-	}
-	
-	@GetMapping("/propiedadDetalle/{id}")
-	public String getPropiedadById(Model model, @PathVariable("id") Integer id) {
-		if(id!=0) {
-			model.addAttribute("listDetalle", propService.get(id));
-		} else {
-			System.out.println("error");
-		}
-		return "propiedadDetalleCliente";
-	}
-	
+
 	@GetMapping("/indexAdmin")
 	public String indexAdmin(Model model, HttpSession session){
 		return "indexAdmin";
 	}
-	
-	@GetMapping("/index")
-	public String index(Model model, HttpSession session) {
+		
+	@RequestMapping("/index")
+	public String listar(Model model) {
+		model.addAttribute("listIndex", propService.getAll());
 		return "index";
 	}
 	
-
 	
+	
+//	@PostMapping("/index")
+//	public List<Propiedad> listarPropiedades(Model model) {
+//		List<Propiedad> propiedades = propService.getAll();
+//		System.out.println("list de propiedades");
+//		System.out.println(propiedades);
+//		return propiedades;	
+//	}
+
 }
